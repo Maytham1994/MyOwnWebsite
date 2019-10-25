5638a8536de6507a542f9ef3ee8777dee84d0c44
Maytham Sabbagh <maythamsabbagh@gmail.com>
2019-10-25 19:44:49 -0400

    Updated Resume and site to October 2019

--- js/custom.js
+++ js/custom.js
@@ -10,7 +10,7 @@
 /*
 * This is the plugin
 */
-(function(a){a.createModal=function(b){defaults={title:"",message:"Maytham Sabbagh Resume January 2017",closeButton:true,scrollable:false};var b=a.extend({},defaults,b);var c=(b.scrollable===true)?'style="max-height: 420px;overflow-y: auto;"':"";html='<div class="modal fade" id="myModal">';html+='<div class="modal-dialog">';html+='<div class="modal-content">';html+='<div class="modal-header">';html+='<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>';if(b.title.length>0){html+='<h4 class="modal-title">'+b.title+"</h4>"}html+="</div>";html+='<div class="modal-body" '+c+">";html+=b.message;html+="</div>";html+='<div class="modal-footer">';if(b.closeButton===true){html+='<button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>'}html+="</div>";html+="</div>";html+="</div>";html+="</div>";a("body").prepend(html);a("#myModal").modal().on("hidden.bs.modal",function(){a(this).remove()})}})(jQuery);
+(function(a){a.createModal=function(b){defaults={title:"",message:"Maytham Sabbagh Resume October 2019",closeButton:true,scrollable:false};var b=a.extend({},defaults,b);var c=(b.scrollable===true)?'style="max-height: 420px;overflow-y: auto;"':"";html='<div class="modal fade" id="myModal">';html+='<div class="modal-dialog">';html+='<div class="modal-content">';html+='<div class="modal-header">';html+='<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>';if(b.title.length>0){html+='<h4 class="modal-title">'+b.title+"</h4>"}html+="</div>";html+='<div class="modal-body" '+c+">";html+=b.message;html+="</div>";html+='<div class="modal-footer">';if(b.closeButton===true){html+='<button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>'}html+="</div>";html+="</div>";html+="</div>";html+="</div>";a("body").prepend(html);a("#myModal").modal().on("hidden.bs.modal",function(){a(this).remove()})}})(jQuery);
 
 /*
 * Here is how you use it
@@ -22,7 +22,7 @@ $(function(){
         //var iframe = '<object data="'+pdf_link+'" type="application/pdf"><embed src="'+pdf_link+'" type="application/pdf" /></object>'        
         var iframe = '<object type="application/pdf" data="'+pdf_link+'" width="100%" height="600">Your browser doesn`t support PDF Viewer <a href="'+pdf_link+'">Download Resume Instead</a></object>'
         $.createModal({
-            title:'Maytham Sabbagh Resume January 2017',
+            title:'Maytham Sabbagh Resume October 2019',
             message: iframe,
             closeButton:true,
             scrollable:false
